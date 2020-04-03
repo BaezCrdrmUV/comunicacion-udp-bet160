310eba00e04210480fef97f32985fea80e7b3991
bet160 <36347319+bet160@users.noreply.github.com>
2020-04-02 21:13:30 -0600

    Proyectos
    Proyectos cliente y servidor

---
+++ Cliente/Cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Cliente
+{
+    class Cliente
+    {
+        static void Main(string[] args)
+        {
+            UdpClient cliente = new UdpClient(9050);
+            string mensaje;
+            byte[] datos = new byte[1024];
+
+            try
+            {
+                cliente.Connect("127.0.0.1", 11000);
+                datos = Encoding.ASCII.GetBytes("Cliente conectado");
+                cliente.Send(datos, datos.Length);
+
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                datos = cliente.Receive(ref endPoint);
+               
+                Console.WriteLine(Encoding.ASCII.GetString(datos, 0, datos.Length));
+
+                while(true){
+
+                    Console.WriteLine("Escriba un mensaje para el servidor");
+                    mensaje = Console.ReadLine().ToString();
+                    datos = Encoding.ASCII.GetBytes(mensaje);
+                    cliente.Send(datos, datos.Length);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ ComunicacionUDP/Servidor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ComunicacionUDP
+{
+    class Servidor
+    {
+        static void Main(string[] args)
+        {
+            byte[] datos = new byte[1024];
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 11000);
+            UdpClient servidor = new UdpClient(endPoint);
+            Console.WriteLine("Esperando la conexion del cliente...");
+            
+            try{
+
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+                datos = servidor.Receive(ref sender);
+
+                Console.WriteLine(Encoding.ASCII.GetString(datos, 0, datos.Length));
+
+                datos = Encoding.ASCII.GetBytes("Conexion lista puede enviar mensajes");
+                servidor.Send(datos, datos.Length, sender);
+
+                while (true)
+                {
+                datos = servidor.Receive(ref sender);
+
+                Console.WriteLine(Encoding.ASCII.GetString(datos, 0, datos.Length));
+                servidor.Send(datos, datos.Length, sender);
+                }
+                
+            }catch(Exception e){
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
